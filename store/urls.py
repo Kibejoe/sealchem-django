d256af219241ae5d91edf5fbf9531d3c591207e3
Kifee <kibejoe254@gmail.com>
2024-09-28 23:19:40 -0700

    store and cart functionality

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.store,name='store'),
-    path('<slug:category_slug>/', views.store, name="products_by_category"), # we use slug coz we want dynamic content
-    path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name="product_detail"), # we use slug coz we want dynamic content
-
+    path('category/<slug:category_slug>/', views.store, name="products_by_category"), # we use slug coz we want dynamic content
+    path('category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name="product_detail"), # we use slug coz we want dynamic content
+    path('search/', views.search, name="search"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from cart.models import Cart, CartItem
+from cart.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
+from django.db.models import Q
+
 
 def store(request, category_slug=None):
     categories = None #When there is no category slug display no categories. So when we refresh, products still show
-    products = None
 
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,  #context is the dictionary passed from the view to the template. The key used in the context will be the key to be used in the template. The value is the result of the query i.e Products.objects.all().filter()
+        'products': paged_products,  #context is the dictionary passed from the view to the template. The key used in the context will be the key to be used in the template. The value is the result of the query i.e Products.objects.all().filter()
         'product_count': product_count
+        
     }
     
     return render(request, 'store/store.html', context)
@@ -26,14 +38,26 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug =category_slug, slug = product_slug) #2 underscores for getting to the slug of the model
+        in_cart = CartItem.objects.filter(cart__cart_id = _cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
     
     context = {
-        'single_product': single_product
+        'single_product': single_product,
+        'in_cart': in_cart
     }
     
     return render(request, 'store/product_detail.html', context)
 
 
-
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword)| Q(product_name__icontains = keyword))
+            product_count = products.count()
+        context = {
+            'products': products,
+            'product_count': product_count
+        }
+    return render(request, 'store/store.html', context)
